083a2cca047d4def3071ea4bc6667a633c9ce1c2
sajeeb5506 <sajeebsarker5506@gmail.com>
2022-05-08 18:45:56 +0600

    404 page added

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Header from './components/Home/Header/Header';
 import Home from './components/Home/Home/Home';
 import Login from './components/Login/Login';
 import Regestation from './components/Regestation/Regestation';
+import Notfound from './NotFound/Notfound';
 import Requiewauth from './RequireAuth/Requiewauth';
 import AddItems from './UserItems/AddItems/AddItems';
 import ManageItem from './UserItems/ManageItems/ManageItem';
@@ -36,10 +37,13 @@ function App() {
       } />
       <Route path="/additems" element={<AddItems />} />
       <Route path="/myitems" element={<MyItems />} />
+
+      <Route path="*" element={<Notfound />} />
       
 
       </Routes>
       <Footer/>
+      
     </div>
   );
 }
---
+++ src/NotFound/Notfound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Notfound.css'
+import img from '../image/404.png'
+const Notfound = () => {
+    return (
+        <div>
+             <img src={img} alt="" />
+        </div>
+    );
+};
+
+export default Notfound;

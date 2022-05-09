82b675d65620913ec4a593c2e19286f29eb2689b
sajeeb5506 <sajeebsarker5506@gmail.com>
2022-05-09 23:47:09 +0600

    all done

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -4,7 +4,7 @@ const Blog = () => {
     return (
         <div>
             <h2 className='text-center my-3'>Difference Between JavaScript and Node JS</h2>
-            <table class="table table-hover">
+            <table className="table table-hover p-5">
   <thead>
     <tr>
     
@@ -37,7 +37,7 @@ const Blog = () => {
 
     
 <h2 className='text-center my-3'>Difference Between SQL and NoSQL</h2>
-            <table class="table table-hover">
+            <table className="table table-hover p-5">
   <thead>
     <tr>
     
@@ -80,6 +80,14 @@ const Blog = () => {
     </tr>
   </tbody>
 </table>
+
+<div className="p-5">
+   <h1 className='my-3'>What is the purpose of JWT</h1>
+   <p>The purpose of JWT which actually is: Getting rid of stateful authentication!
+JSON stands for JavaScript Object Notation and could be a text-based format for transmitting data across web applications. It stores information in an easy-to-access manner, both for developers and computers. It are often used as a knowledge format by any programing language and is quickly becoming the popular syntax for APIs, surpassing XML.
+
+JWTs are used as a secure way to authenticate users and share information. Typically, a private key, or secret, is used by the issuer to sign the JWT. The receiver of the JWT will verify the signature to ensure that the token hasn’t been altered after it was signed by the issuer. It is difficult for unauthenticated sources to guess the signing key and attempt to change the claims within the JWT. Not all signing algorithms are created equal though. For example, some signing algorithms use a secret value that is shared between the issuer and the party that verifies the JWT. Other algorithms use a public and private key. The private key is known only to the issuer, while the public key can be widely distributed. The public key can be used to verify the signature, but only the private key can be used to create the signature. This is more secure than a shared secret because the private key only needs to exist in one place. Because of this, the server does not need to keep a database with the information needed to identify the user. For developers, this is great news — the server that issues the JWT and the server that validates it do not have to be the same.</p>
+</div>
         </div>
     );
 };
